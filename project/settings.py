4914447acd305e70d249fb6056b52fa76ea6bf5a
Devin Mork <devin.mork@gmail.com>
2024-04-09 08:42:43 -0500

    Configure for dj-database-url

--- project/settings.py
+++ project/settings.py
@@ -83,12 +83,9 @@ class Dev(Configuration):
     # Database
     # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+    DATABASES = values.DatabaseURLValue(
+        f'sqlite:///{BASE_DIR}/db.sqlite3', environ_prefix='DJANGO'
+    )
 
 
     # Password validation
