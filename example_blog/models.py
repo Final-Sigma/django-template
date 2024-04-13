4820d7412563c08098116d0887940212efc18706
Devin Mork <devin.mork@gmail.com>
2024-04-13 06:41:29 -0500

    Finish blog CRUD creation

--- example_blog/models.py
+++ example_blog/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from django.urls import reverse
 
 class BlogPost(models.Model):
     title = models.CharField(
         max_length=50
     )
     body = models.TextField()
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def get_absolute_url(self):
+        return reverse('blog:post', kwargs={'pk': self.pk})
+    
+    def __str__(self):
+        return f'{self.title}'
--- example_blog/views.py
+++ example_blog/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.views.generic.edit import FormView, CreateView
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
 from .forms import PostForm
 from .models import BlogPost as Post
 
-class PostCreate(CreateView):
+class PostsIndex(ListView):
+    model = Post
+
+class PostsCreate(CreateView):
     # from FormMixin which FormView inherits.
     # initial = {}
     # form_class = None
@@ -12,3 +18,14 @@ class PostCreate(CreateView):
     model = Post
     form_class = PostForm
     # template_name = 'example_blog/blogpost_form.html'
+
+class PostsDetail(DetailView):
+    model = Post
+
+class PostsUpdate(UpdateView):
+    model = Post
+    form_class = PostForm
+
+class PostsDelete(DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:posts')
